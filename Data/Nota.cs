e2bb95e71718d0d9d8dd25e60ba81770873b5638
Andrewlwm <geo.bal.2000@gmail.com>
2020-12-14 11:23:46 +0200

    misc

--- Data/Nota.cs
+++ Data/Nota.cs
@@ -4,28 +4,28 @@ using ServiceStack.DataAnnotations;
 
 namespace ProiectPbd.Data
 {
-  [Alias("Note")]
-  public class Nota
-  {
-    [AutoIncrement]
-    public int Id { get; set; }
+    [Alias("Note")]
+    public class Nota
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
 
-    [ForeignKey(typeof(Student), OnDelete = "CASCADE", OnUpdate = "CASCADE")]
-    [Required]
-    public int StudentId { get; set; }
+        [ForeignKey(typeof(Student), OnDelete = "CASCADE", OnUpdate = "CASCADE")]
+        [Required]
+        public int StudentId { get; set; }
 
-    [ForeignKey(typeof(Materie), OnDelete = "CASCADE", OnUpdate = "CASCADE")]
-    [Required]
-    public int MaterieId { get; set; }
+        [ForeignKey(typeof(Materie), OnDelete = "CASCADE", OnUpdate = "CASCADE")]
+        [Required]
+        public int MaterieId { get; set; }
 
-    [Required]
-    public int NrPrezentare { get; set; }
+        [Required]
+        public int NrPrezentare { get; set; }
 
-    [Required]
-    public DateTime DataPrezentarii { get; set; }
+        [Required]
+        public DateTime DataPrezentarii { get; set; }
 
-    [Required]
-    [CheckConstraint("NotaObtinuta BETWEEN 1 AND 10")]
-    public int NotaObtinuta { get; set; }
-  }
+        [Required]
+        [CheckConstraint("NotaObtinuta BETWEEN 1 AND 10")]
+        public int NotaObtinuta { get; set; }
+    }
 }
